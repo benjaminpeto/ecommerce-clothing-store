240b1337159488b853d9a782c6de90d38cc3f79d
Benji <benjaminpeto23@gmail.com>
2021-11-18 14:38:45 +0100

    moved shop.data.js to firebase, laveraging with higher order components

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -44,13 +44,45 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
 firebase.initializeApp(config);
 
+// Exporting shop.data.js to Firebase - only once
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+    console.log(collectionRef);
+
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj);
+    });
+
+    return await batch.commit();     //firing batch request
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const tranformedCollection = collections.docs.map(doc => {
+        const { title, items } = doc.data();
+
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        }
+    });
+
+    return tranformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    }, {});
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account '});
 /* export const signInWithGoogle = () => auth.signInWithPopup(provider); */ //popup box error on closing
-export const signInWithGoogle = () => 
-  signInWithPopup(auth, provider).catch((error) => console.log(error));
+export const signInWithGoogle = () => signInWithPopup(auth, provider).catch((error) => console.log(error));
 
 export default firebase;
