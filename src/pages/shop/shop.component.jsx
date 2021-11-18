6d8747d3b58fb26bca81bed8b79f0c302afa9e6b
Benji <benjaminpeto23@gmail.com>
2021-11-18 16:03:43 +0100

    changed promise method

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -23,11 +23,12 @@ class ShopPage extends React.Component {
 	componentDidMount() {
 		const { updateCollections } = this.props;
 		const collectionRef = firestore.collection('collections');
-		
-		collectionRef.onSnapshot(async snapshot => {
-			const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-			updateCollections(collectionsMap);
-			this.setState({ loading: false });
+
+		collectionRef.get()
+			.then(snapshot => {
+				const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+				updateCollections(collectionsMap);
+				this.setState({ loading: false });
 		});
 	};
 
