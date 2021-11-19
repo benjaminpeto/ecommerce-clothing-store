007ec2edb71636deae406b9d8bd6ba6d3ddbf425
Benji <benjaminpeto23@gmail.com>
2021-11-19 15:39:12 +0100

    refactored collection component and shop page to avoid the pollution of the namspace by introduced containers to existing components, also took advantage of compose from redux to write the code cleaner without wrapping too many components

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import { createStructuredSelector } from 'reselect';
 import { connect } from "react-redux";
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from "../collection/collection.component";
-
 import { fetchCollectionStartAsync } from "../../redux/shop/shop.actions";
-import { selectIsCollectionFetching, selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors';
 
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
-
-const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
+import CollectionPageContainer from "../collection/collection.container";
 
 class ShopPage extends React.Component {
 
@@ -22,34 +15,29 @@ class ShopPage extends React.Component {
 	};
 
 	render() {
-		const { match, isCollectionFetching, isCollectionsLoaded } = this.props;
+		const { match } = this.props;
 
 		return (
 			<div className="shop-page">
 				<Route
 					exact
 					path={`${match.path}`}
-					render={(props) => <CollectionsOverviewWithSpinner isLoading={isCollectionFetching} {...props} />} 
+					component={CollectionsOverviewContainer}
 				/>
 				<Route
 					path={`${match.path}/:collectionId`}
-					render={(props) => <CollectionPageWithSpinner isLoading={!isCollectionsLoaded} {...props} />} 
+					component={CollectionPageContainer}
 				/>
 			</div>
 		);
 	}
 };
 
-const mapStateToProps = createStructuredSelector({
-	isCollectionFetching: selectIsCollectionFetching,
-	isCollectionsLoaded: selectIsCollectionsLoaded
-});
-
 const mapDispatchedToProps = dispatch => ({
 	fetchCollectionStartAsync: () => dispatch(fetchCollectionStartAsync())
 });
 
 export default connect(
-	mapStateToProps,
+	null,
 	mapDispatchedToProps
 )(ShopPage);
